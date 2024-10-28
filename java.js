94ac449637dae3233317fb5dae7365a96bab961a
Pineapple4u <165797715+Pineapple4u@users.noreply.github.com>
2024-10-28 11:02:19 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+var b = 3431;
+var c = 8724;
+var bc = b*c;
+var q = prompt("What is 3431 x 8724? :  ");
+var q2 = q.trim();
+
+if (bc = q2){
+    console.log("Correct!")
+}
+
+else{
+    console.log("Oops.. try again")
+}
+
+
+
+
+
+
